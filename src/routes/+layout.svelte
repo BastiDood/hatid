632dad4031e5ac3dfd361e15de6fabba8cfb859e
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-07-20 13:41:14 +0800

    deps: upgrade major versions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,7 @@
 <script lang="ts">
-    // The ordering of these imports is critical to your app working properly
     import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-    // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
     import '@skeletonlabs/skeleton/styles/skeleton.css';
+    import '../app.css';
 </script>
 
 <slot />
