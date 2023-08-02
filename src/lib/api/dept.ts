4ecb4ca60b88bdede72ea7d5d1502d14665b1f4e
Hascion <hssalazar@up.edu.ph>
2023-08-02 13:39:30 +0800

    feat(api): implement department renaming

--- src/lib/api/dept.ts
+++ src/lib/api/dept.ts
@@ -22,3 +22,26 @@ export async function create(name: Dept['name']) {
             throw new UnexpectedStatusCode(res.status);
     }
 }
+
+/** Edits the `name` field of a {@linkcode Dept}. Returns `false` if not found. */
+export async function editName(did: Dept['dept_id'], name: Dept['name']){
+    const res = await fetch('/api/dept/name', {
+        method: 'PATCH',
+        credentials: 'same-origin',
+        body: new URLSearchParams({ id: did.toString(10), name }),
+    });
+    switch (res.status) {
+        case StatusCodes.NO_CONTENT:
+            return true;
+        case StatusCodes.NOT_FOUND:
+            return false;
+        case StatusCodes.BAD_REQUEST:
+            throw new BadInput();
+        case StatusCodes.UNAUTHORIZED:
+            throw new InvalidSession();
+        case StatusCodes.FORBIDDEN:
+            throw new InsufficientPermissions();
+        default:
+            throw new UnexpectedStatusCode(res.status);
+    } 
+}
