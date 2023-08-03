40d1d720258619830a166b25239dde60a926bca2
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-03 20:21:59 +0800

    chore: run formatter

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -10,10 +10,10 @@
 
 <AppShell>
     <AppBar slot="header">
-        <Avatar src={picture} width="w-8" slot="lead" />
+        <Avatar src="{picture}" width="w-8" slot="lead" />
         {name} &lt;{email}&gt;
     </AppBar>
-	<svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
-    <svelte:fragment slot="footer"></svelte:fragment>
+    <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment>
+    <svelte:fragment slot="footer" />
     <slot />
 </AppShell>
