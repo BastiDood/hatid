cc4b65ab9b8085cdc15605085f30ec6959a3cd00
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-05 18:56:02 +0800

    feat(api): implement label subscription

--- src/lib/api/dept.ts
+++ src/lib/api/dept.ts
@@ -1,5 +1,5 @@
 import { BadInput, InsufficientPermissions, InvalidSession, UnexpectedStatusCode } from './error';
-import { type Dept, DeptSchema } from '$lib/model/dept';
+import { type Dept, type DeptLabel, DeptSchema } from '$lib/model/dept';
 import { StatusCodes } from 'http-status-codes';
 
 /** Creates a new {@linkcode Dept} and returns the ID. */
@@ -35,6 +35,35 @@ export async function editName(did: Dept['dept_id'], name: Dept['name']) {
             return true;
         case StatusCodes.NOT_FOUND:
             return false;
+        case StatusCodes.BAD_REQUEST:
+            throw new BadInput();
+        case StatusCodes.UNAUTHORIZED:
+            throw new InvalidSession();
+        case StatusCodes.FORBIDDEN:
+            throw new InsufficientPermissions();
+        default:
+            throw new UnexpectedStatusCode(status);
+    }
+}
+
+/**
+ * Subscribes a {@linkcode Dept} to a {@linkcode Label}. If any of `did` and `lid` do not exist,
+ * this function returns `null`. Returns `true` if successfully created and `false` otherwise if
+ * the `did`-`lid` pair already exists in the database (in which case nothing is done).
+ */
+export async function subscribeToLabel(did: DeptLabel['dept_id'], lid: DeptLabel['label_id']) {
+    const { status } = await fetch('/api/dept/label', {
+        method: 'POST',
+        credentials: 'same-origin',
+        body: new URLSearchParams({ dept: did.toString(10), label: lid.toString(10) }),
+    });
+    switch (status) {
+        case StatusCodes.CREATED:
+            return true;
+        case StatusCodes.CONFLICT:
+            return false;
+        case StatusCodes.NOT_FOUND:
+            return null;
         case StatusCodes.BAD_REQUEST:
             throw new BadInput();
         case StatusCodes.UNAUTHORIZED:
