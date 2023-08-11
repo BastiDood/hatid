d938013c95d1590c5e43144d1894e7b88977a5ab
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-12 03:16:34 +0800

    fix: ensure all hex colors are unsigned

--- src/lib/model/label.ts
+++ src/lib/model/label.ts
@@ -3,7 +3,10 @@ import { z } from 'zod';
 export const LabelSchema = z.object({
     label_id: z.number().int().positive(),
     title: z.string().max(32),
-    color: z.number().int(),
+    color: z
+        .number()
+        .int()
+        .transform(hex => hex >>> 0),
     deadline: z.number().int().positive().nullable(),
 });
 
