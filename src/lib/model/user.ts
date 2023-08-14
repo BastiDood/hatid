b25e6afab1ab48c0318fabcfe1ef8dabfc70d27f
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-14 17:56:07 +0800

    fix(model): assert format for user email

--- src/lib/model/user.ts
+++ src/lib/model/user.ts
@@ -5,7 +5,7 @@ export const GoogleUserId = z.string().min(1).max(255);
 export const UserSchema = z.object({
     user_id: GoogleUserId,
     name: z.string().max(64),
-    email: z.string().max(40),
+    email: z.string().email().max(40),
     picture: z.string().url(),
     admin: z.coerce.boolean(),
 });
