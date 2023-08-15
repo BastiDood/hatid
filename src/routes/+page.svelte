f59309474401df9c4235e51560e66dbaa75321a7
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-08-15 19:44:47 +0800

    refactor: remove copyright line temporarily

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -127,13 +127,4 @@
     <!--
 	<AppBar slot="footer">Footer</AppBar> -->
     <!-- (footer) -->
-
-    <footer class="bg-neutral-200 text-center dark:bg-neutral-700 lg:text-left">
-        <div class="p-4 text-center text-neutral-700 dark:text-neutral-200">
-            © 2023 Copyright:
-            <a class="text-neutral-800 dark:text-neutral-400" href="https://tailwind-elements.com/"
-                >Tailwind Elements</a
-            >
-        </div>
-    </footer>
 </AppShell>
