1f059d1f399ba3ac0f61b6e7f13c135420f550a4
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-15 16:31:40 +0800

    chore: rename some configuration variables

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import node from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,5 +6,5 @@ export default {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors.
     preprocess: vitePreprocess(),
-    kit: { adapter: adapter() },
+    kit: { adapter: node() },
 };
