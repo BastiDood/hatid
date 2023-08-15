24dd80b79cfd6cb29c59df8b5c192c57fc90b4f8
Anjellyrika <98273014+Anjellyrika@users.noreply.github.com>
2023-08-15 19:28:14 +0800

    refactor: move `app.css` to `routes` folder
    Co-authored-by: Basti Ortiz <39114273+BastiDood@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
     import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
     import '@skeletonlabs/skeleton/styles/skeleton.css';
-    import '../app.css';
+    import './app.css';
 </script>
 
 <slot />
