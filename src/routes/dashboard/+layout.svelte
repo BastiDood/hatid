98bfd261118546fd4d4ac5a9bf679d325bcb6fe5
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-18 04:36:43 +0800

    feat(ui): enable `selected` style for app rail tiles

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -20,6 +20,9 @@
     import Tag from '@krowten/svelte-heroicons/icons/TagIcon.svelte';
     import Users from '@krowten/svelte-heroicons/icons/UsersIcon.svelte';
     import logo from '$lib/images/HATiD.png';
+    import { page } from '$app/stores';
+
+    $: ({ pathname } = $page.url);
 
     // eslint-disable-next-line init-declarations
     export let data: LayoutServerData;
@@ -43,25 +46,28 @@
             <Avatar src="{picture}" class="w-8" />
         </svelte:fragment>
     </AppBar>
-    <AppRail slot="sidebarLeft" width="w-24" regionTrail="mb-4">
-        <AppRailAnchor href="/dashboard/inbox">
+    <AppRail slot="sidebarLeft" width="w-24">
+        <AppRailAnchor href="/dashboard/inbox" selected="{pathname.startsWith('/dashboard/inbox')}">
             <Inbox slot="lead" class="h-8 w-8" solid />
             <span>Inbox</span>
         </AppRailAnchor>
         <!-- TODO: Use `data.admin` to remove this for non-admin people. -->
-        <AppRailAnchor href="/dashboard/priority">
+        <AppRailAnchor
+            href="/dashboard/priority"
+            selected="{pathname.startsWith('/dashboard/priority')}"
+        >
             <QueueList slot="lead" class="h-8 w-8" solid />
             <span>Priorities</span>
         </AppRailAnchor>
-        <AppRailAnchor href="/dashboard/label">
+        <AppRailAnchor href="/dashboard/label" selected="{pathname.startsWith('/dashboard/label')}">
             <Tag slot="lead" class="h-8 w-8" solid />
             <span>Labels</span>
         </AppRailAnchor>
-        <AppRailAnchor href="/dashboard/user">
+        <AppRailAnchor href="/dashboard/user" selected="{pathname.startsWith('/dashboard/user')}">
             <Users slot="lead" class="h-8 w-8" solid />
             <span>Users</span>
         </AppRailAnchor>
-        <AppRailAnchor href="/dashboard/dept">
+        <AppRailAnchor href="/dashboard/dept" selected="{pathname.startsWith('/dashboard/dept')}">
             <BuildingOffice slot="lead" class="h-8 w-8" solid />
             <span>Departments</span>
         </AppRailAnchor>
@@ -69,7 +75,7 @@
         <button
             type="button"
             slot="trail"
-            class="flex w-full appearance-none flex-col items-center justify-center gap-1"
+            class="flex aspect-square w-full appearance-none flex-col items-center justify-center gap-1 hover:bg-error-hover-token active:bg-error-active-token"
         >
             <Logout class="block h-8 w-8" />
             <span class="app-rail-label text-xs font-bold">Logout</span>
