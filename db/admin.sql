48f2b722a7f6e2bd18f1cd1551f478deb9e4b0ea
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-17 22:31:20 +0800

    chore(db): simplify prompt for admin

--- db/admin.sql
+++ db/admin.sql
@@ -1,5 +1,5 @@
 SELECT user_id, name, email FROM users;
 
-\prompt 'Who do you want to promote as a system administrator? Insert the user ID. ' uid
+\prompt 'Which user ID would you like to promote as a system adminstrator? ' uid
 
 UPDATE users SET admin = TRUE WHERE user_id = :uid::TEXT;
