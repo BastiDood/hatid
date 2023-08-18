f85d08fc0deea842117ceb92c66c5cb106ee2565
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-18 15:01:41 +0800

    fix(db): resolve typo in admin script

--- db/admin.sql
+++ db/admin.sql
@@ -1,5 +1,5 @@
 SELECT user_id, name, email FROM users WHERE NOT admin;
 
-\prompt 'Which user ID would you like to promote as a system adminstrator? ' uid
+\prompt 'Which user ID would you like to promote as a system administrator? ' uid
 
 UPDATE users SET admin = TRUE WHERE user_id = :uid::TEXT;
