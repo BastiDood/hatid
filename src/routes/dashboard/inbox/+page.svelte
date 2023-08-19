cede03dbf5b973f941246a6e86edf9bce837ebac
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-20 01:12:38 +0800

    feat(ui): link to the user and agent inbox

--- src/routes/dashboard/inbox/+page.svelte
+++ src/routes/dashboard/inbox/+page.svelte
@@ -1,6 +1,16 @@
 <script>
+    import { InboxIcon as Inbox, InboxStackIcon as InboxStack } from '@krowten/svelte-heroicons';
     import OpenTicket from './OpenTicket.svelte';
 </script>
 
 <OpenTicket />
-<!-- TODO: Let the user select from their personal inbox and agent inbox. -->
+<div class="logo-cloud flex flex-row gap-1">
+    <a href="/dashboard/inbox/user" class="logo-item">
+        <Inbox class="h-8 w-8" />
+        <span>Your Tickets</span>
+    </a>
+    <a href="/dashboard/inbox/agent" class="logo-item">
+        <InboxStack class="h-8 w-8" />
+        <span>Assigned to You</span>
+    </a>
+</div>
