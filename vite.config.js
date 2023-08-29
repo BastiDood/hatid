83c330cdc67508cc60254e02e5d49a32f1643464
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2023-08-29 18:34:23 +0800

    chore: update theme configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vitest/config';
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    test: {
-        include: ['src/**/*.{test,spec}.{js,ts}'],
-    },
+    plugins: [sveltekit(), purgeCss()],
+    test: { include: ['src/**/*.{test,spec}.{js,ts}'] },
 });
